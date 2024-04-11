62efcbed47410adbb6569875b8e3a60354c41419
Alex <83400968+CuriosityAHumanSuperpower@users.noreply.github.com>
2024-04-11 16:23:50 +0000

    add packages

--- server.mjs
+++ server.mjs
@@ -20,11 +20,9 @@ var root = {
 var app = express();
 
 app.use('/graphql', createHandler({
-    schema: schema,  // Must be provided
+    schema,  // Must be provided
     rootValue: root,
     graphiql: true,  // Enable GraphiQL when server endpoint is accessed in browser
   }));
 
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
-
-
