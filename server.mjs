657b99e7a34afd295c788ef0f31a19ffe1e3e623
Alex <83400968+CuriosityAHumanSuperpower@users.noreply.github.com>
2024-04-11 16:09:16 +0000

    init server with express and graphql

---
+++ server.mjs
@@ -0,0 +1,30 @@
+import express from "express" 
+import { createHandler } from "graphql-http/lib/use/express"; 
+import { buildSchema } from "graphql"
+
+// Initialize a GraphQL schema
+
+var schema = buildSchema(`
+    type Query { 
+        hello : String
+    }
+`)
+
+// Root resolver
+
+var root = { 
+    hello : () => 'Hello word !'
+}
+
+// Create an express server with a GraphQL endpoint
+var app = express();
+
+app.use('/graphql', createHandler({
+    schema: schema,  // Must be provided
+    rootValue: root,
+    graphiql: true,  // Enable GraphiQL when server endpoint is accessed in browser
+  }));
+
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+
+
